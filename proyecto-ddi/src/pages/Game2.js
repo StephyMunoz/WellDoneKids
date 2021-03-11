51eea43fdaba4d2de868513975aa2702fccb32a3
stiven212 <stivfunk2000@hotmail.com>
2021-03-11 08:58:51 -0500

    instalación de prettier

--- proyecto-ddi/src/pages/Game2.js
+++ proyecto-ddi/src/pages/Game2.js
@@ -1,47 +1,46 @@
-import React from 'react';
-import '../styles/game.css';
-import {Button, Col, Row} from "antd";
+import React from "react";
+import "../styles/game.css";
+import { Button, Col, Row } from "antd";
 import GameNav from "../components/GameNav";
-import ReactPlayer from 'react-player';
+import ReactPlayer from "react-player";
 import Routes from "../constants/Routes";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import withAuth from "../hocs/withAuth";
 
 const Game2 = () => {
-
-    return(
-        <div className="Game">
-            <GameNav />
-            <Row justify='center'>
-                <Col justify='center'>
-                    <h1>Buen trabajo USERNAME!!!</h1>
-                    <h1>Disfruta este video que hemos</h1>
-                    <h1>preparado para ti.</h1>
-                    <h1>Te lo has ganado</h1>
-                </Col>
-            </Row>
-            <Row justify='center'>
-                <Col>
-                    <div>
-                        <ReactPlayer
-                            url='https://www.youtube.com/watch?v=W08ToPMihhw'
-                            className='react-player'
-                            playing
-                            width='60em'
-                            height='30em'
-                            controls='true'
-                        />
-                    </div>
-                </Col>
-            </Row>
-            <Row justify='center'>
-                <Col>
-                    <Link to={Routes.GAME3}>
-                        <Button type="primary" >CONTINUAR :)</Button>
-                    </Link>
-                </Col>
-            </Row>
-        </div>
-    );
-}
+  return (
+    <div className="Game">
+      <GameNav />
+      <Row justify="center">
+        <Col justify="center">
+          <h1>Buen trabajo USERNAME!!!</h1>
+          <h1>Disfruta este video que hemos</h1>
+          <h1>preparado para ti.</h1>
+          <h1>Te lo has ganado</h1>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <div>
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=W08ToPMihhw"
+              className="react-player"
+              playing
+              width="60em"
+              height="30em"
+              controls="true"
+            />
+          </div>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <Link to={Routes.GAME3}>
+            <Button type="primary">CONTINUAR :)</Button>
+          </Link>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 export default withAuth(Game2);
--- proyecto-ddi/src/pages/Game3.js
+++ proyecto-ddi/src/pages/Game3.js
@@ -1,36 +1,34 @@
-import React from 'react';
-import '../styles/game.css';
-import {Button, Col, Row} from "antd";
+import React from "react";
+import "../styles/game.css";
+import { Button, Col, Row } from "antd";
 import GameNav from "../components/GameNav";
 import Routes from "../constants/Routes";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import withAuth from "../hocs/withAuth";
 
 const Game3 = () => {
-
-    return(
-        <div className="Game">
-            <GameNav />
-            <Row justify='center'>
-                <Col justify='center'>
-                    <h1>La aventura ha</h1>
-                    <h1>terminado... por ahora.</h1>
-
-                </Col>
-            </Row>
-            <Row justify='center'>
-                <Col>
-                    <Button type="primary" >SALIR :(</Button>
-                </Col>
-            </Row>
-            <Row justify='center'>
-                <Col>
-                    <Link to={Routes.USERSPROFILE}>
-                        <Button type="primary" >VER LOGROS :)</Button>
-                    </Link>
-                </Col>
-            </Row>
-        </div>
-    );
-}
+  return (
+    <div className="Game">
+      <GameNav />
+      <Row justify="center">
+        <Col justify="center">
+          <h1>La aventura ha</h1>
+          <h1>terminado... por ahora.</h1>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <Button type="primary">SALIR :(</Button>
+        </Col>
+      </Row>
+      <Row justify="center">
+        <Col>
+          <Link to={Routes.USERSPROFILE}>
+            <Button type="primary">VER LOGROS :)</Button>
+          </Link>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 export default withAuth(Game3);
