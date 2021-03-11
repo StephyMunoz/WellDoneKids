241276afd4779320fcd79df5c342aa3a4931ee19
StephyMunoz <58042439+StephyMunoz@users.noreply.github.com>
2021-03-11 10:43:14 -0500

    credenciales de firebase funcionando

---
+++ proyecto-ddi/src/components/Questions.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {useEffect, useState} from 'react';
+import '../styles/game.css';
+import {Button, Col, Input, Row, Radio} from "antd";
+import GameNav from "../components/GameNav";
+import Routes from "../constants/Routes";
+import {Link} from "react-router-dom";
+import withAuth from "../hocs/withAuth";
+import {db} from '../firebase';
+
+
+const Questions = () => {
+
+}
+export default Questions;
