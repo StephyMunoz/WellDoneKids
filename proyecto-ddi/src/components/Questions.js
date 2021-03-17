ab8798fc90c0f402052d82e0013b617bd2d89942
Mateo Borja <mateo.borja@epn.edu.ec>
2021-03-16 19:48:21 -0500

    actualizacion de puntajes en la base (defectuoso)

--- proyecto-ddi/src/components/Questions.js
+++ proyecto-ddi/src/components/Questions.js
@@ -13,15 +13,16 @@ import { Username } from "../components/Username";
 import { useAuth } from "../lib/auth";
 
 const Questions = ({ selectSubject }) => {
+  const { user } = useAuth();
   const [questionNumber, setQuestionNumber] = useState(1);
   const [questionList, setQuestionList] = useState([]);
   const [radioState, setRadioState] = useState(0);
-  const [score, setScore] = useState(0);
+  var [gameScore, setGameScore] = useState(user.score);
+  const [gameMistakes, setGameMistakes] = useState(user.mistakes);
   //const [subject, setSubject] = useState(0);
-  const { username } = Username();
-  const {user} = useAuth();
-  
-  let number = -1; // English
+
+  let number = -1;
+  let age = 0;
 
   const onChange = (e) => {
     setRadioState({
@@ -43,33 +44,44 @@ const Questions = ({ selectSubject }) => {
         console.log("dlan", number);
       }
 
+      if (user.selectedYear === 6 || user.selectedYear === 7) {
+        age = 0;
+      } else if (user.selectedYear === 8 || user.selectedYear === 9) {
+        age = 1
+      } else {
+        age = 2
+      }
+
       //if (number >= 0) {
-      db.ref(`subjects/${number}/questions/0`).on(
-        "value",
-        (snapshot) => {
-          const questions = [];
-          snapshot.forEach((question) => {
-            const q = question.val();
-            questions.push(q);
-          });
-          setQuestionList(questions);
-          console.log("questions", questions);
-          console.log("subjects/" + number + "/questions/0/question");
-          // setSubject(selecSubject);
-          console.log('username', username);
-        }
-      );
+      db.ref(`subjects/${number}/questions/${age}`).on("value", (snapshot) => {
+        const questions = [];
+        snapshot.forEach((question) => {
+          const q = question.val();
+          questions.push(q);
+        });
+        setQuestionList(questions);
+        // setSubject(selecSubject);
+      });
       // }
     };
     getQuestions();
     return () => {
-      db.ref(`subjects/${number}/questions/0/question`).off();
+      db.ref(`subjects/${number}/questions/${age}`).off();
     };
   }, []);
 
-  const uploadScore = (score) => {
-    db.ref(`users`)
-  }
+  const uploadScore = (gameScore) => {
+    db.ref(`users/${user.uid}/score`).set(gameScore);
+  };
+
+  const uploadMistakes = (gameMistakes) => {
+    db.ref(`users/${user.uid}/mistakes`).set(user.mistakes + gameMistakes);
+  };
+
+  const uploadScores = (gameScore, gameMistakes) => {
+    uploadScore(gameScore);
+    uploadMistakes(gameMistakes);
+  };
 
   const radioStyle = {
     display: "block",
@@ -80,17 +92,17 @@ const Questions = ({ selectSubject }) => {
   const handleQuestionChange = (questionNumber) => {
     console.log(value);
     if (value === questionList[questionNumber].correct_answer) {
-      setScore(score + 1);
+      setGameScore(gameScore + 1);
+      uploadScore(gameScore);
       setQuestionNumber(questionNumber + 1);
+      console.log("user score", user.score);
 
       setQuestionNumber(random);
-      console.log("opcion seleccionada", radioState);
-      console.log("puntaje", score);
-      console.log("numero pregunta", questionNumber);
+      console.log("puntaje", gameScore);
     } else {
       console.log("respuesta incorrecta");
-
       alert("Respuesta incorrecta. Intentalo de nuevo :)");
+      setGameMistakes(gameMistakes + 1);
       console.log("sub from ques", selectSubject);
     }
   };
@@ -98,13 +110,13 @@ const Questions = ({ selectSubject }) => {
 
   return (
     <>
-      {score <= 10 ? (
+      {gameScore < 10 ? (
         questionList.length > 0 ? (
           <div className="Game">
             <GameNav />
             <Row>
               <Col span={4} offset={4}>
-                <h1>Puntaje: {score}</h1>
+                <h1>Puntaje: {gameScore}</h1>
               </Col>
             </Row>
             <Row justify="center">
